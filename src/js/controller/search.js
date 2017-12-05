18af56a0a38880dc13cd39a2c6e56c7807375195
pirolo <fmolinalopez@eci.safanet.es>
2017-12-05 02:59:39 +0100

    Terminado getRecipesWithMostSteps y su vista

--- src/js/controller/search.js
+++ src/js/controller/search.js
@@ -1,6 +1,7 @@
 const getRecipesByNameOrSteps = require("../pages/search/getRecipesByNameOrSteps");
 const getRecipesBySimilarRecipe = require("../pages/search/getRecipesBySimilarRecipe");
 const getRecipesByTypeOfFood = require("../pages/search/getRecipesByTypeOfFood");
+const getRecipesWithMostSteps = require("../pages/trivia/getRecipesWithMostSteps");
 const recipeToDiv = require("../support/recipeToDiv");
 const { getState } = require("../appState");
 
@@ -105,10 +106,46 @@ function loadSearchByTypeOfFood() {
 	});
 }
 
+function loadSearchRecipesWithMostSteps() {
+	const button = qs("#search-recipes-with-most-steps-button");
+	const input = qs("#search-by-foodType-input");
+
+	button.addEventListener("click", () => {
+		const foodType = input.value.trim().replace(/\s{2,}/g, " ").toLowerCase();
+
+		if (typeof foodType !== "string") {
+			return;
+		}
+
+		const results = getRecipesWithMostSteps(foodType);
+
+
+		// Borrar los resultados de una búsqueda anterior
+		while (resultsOutput.firstChild) {
+			resultsOutput.removeChild(resultsOutput.firstChild);
+		}
+
+		if (results.length === 0) {
+			const p = document.createElement("p");
+			p.textContent = "No se han encontrado recetas";
+
+			resultsOutput.appendChild(p);
+		} else {
+			results
+				.map(recipeToDiv)
+				.forEach(it => resultsOutput.appendChild(it));
+		}
+
+		// Mostrar los resultados
+		resultsDiv.style.display = "block";
+	});
+}
+
 function loadSearchController() {
 	loadSearchByNameOrSteps();
 	loadSearchBySimilarRecipe();
 	loadSearchByTypeOfFood();
+	loadSearchRecipesWithMostSteps();
 }
 
 module.exports = loadSearchController;
