cab69ce233dbfac187e9504c1fb28cb63e84e4e9
pirolo <fmolinalopez@eci.safanet.es>
2017-12-12 02:58:18 +0100

    Añadidos test pendientes

--- src/js/pages/trivia/getMostUsedIngredient.spec.js
+++ src/js/pages/trivia/getMostUsedIngredient.spec.js
@@ -1,3 +1,10 @@
-/**
- * Created by Vikin on 12/12/2017.
- */
+describe("getMostUsedIngredient", () => {
+	it("deberia devolver el ingrediente mas usado de todas las recetas si no" +
+		" recibe parametro");
+	it("si se le introduce un tipo de comida debera devolver el ingrediente" +
+		" mas usado en ese tipo de comida");
+	it("en caso de que no exista el tipo de comida recibido debe devolver una" +
+		" cadena vacia");
+	it("debe devolver un string con el nombre del ingrediente");
+});
+
