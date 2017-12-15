badd4be5e5761e3e0edfb10d909e98533580c81c
pirolo <fmolinalopez@eci.safanet.es>
2017-12-15 20:19:41 +0100

    Creado Login de usuarios

---
+++ src/js/pages/users/login.spec.js
@@ -0,0 +1,5 @@
+describe("logIn(username, password)", () => {
+	it("deberia logear al usuario si recibe un usuario y contraseña correctos");
+	it(" no deberia logear al usuario si recibe un usuario o contraseña" +
+		" incorrectos");
+});
