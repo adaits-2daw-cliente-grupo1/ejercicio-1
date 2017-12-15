ed5037272636cd558f7d0f797e4a6a18b9afd959
Juan Luis Romero <juanlu_21@hotmail.com>
2017-12-15 22:24:17 +0100

    comentada mimificacion de código y intento de arreglar función

--- src/js/controller/search/loadSearchRecipesRatedByUser.js
+++ src/js/controller/search/loadSearchRecipesRatedByUser.js
@@ -6,38 +6,43 @@ const qs = document.querySelector.bind(document);
 
 const resultsDiv = qs(".results");
 
+function marcarError() {
+	const numeroInvalido = "El número introducido no es un número válido";
+	const input = $("#search-recipes-rated-by-user-input");
+	if (input.next().length === 0) {
+		("#search-recipes-rated-by-user-input").after("<p class='error'>No se ha añadido un número" +
+			" válido</p>");
+		throw new Error(numeroInvalido);
+	} else {
+		input.next().remove();
+		input.after("<p class='error'>No se ha añadido un número" +
+			" válido</p>");
+		throw new Error(numeroInvalido);
+	}
+}
+
 function loadSearchRecipesRatedByUser() {
 	$("#search-recipes-rated-by-user-button").click(() => {
-		const numeroInvalido = "El número introducido no es un número válido";
 		let results;
-		const valor = parseInt($("#search-recipes-rated-by-user-input").val(), 10);
+		const input = $("#search-recipes-rated-by-user-input");
+		const valor = parseInt(input.val(), 10);
 		if (typeof valor !== typeof NaN) {
 			results = getRecipesRatedByUser(valor);
-		} else {
-			if ($("#search-recipes-rated-by-user-input").next().length === 0) {
-				("#search-recipes-rated-by-user-input").after("<p class='error'>No se ha añadido un número" +
-					" válido</p>");
-				throw new Error(numeroInvalido);
-			} else {
-				$("#search-recipes-rated-by-user-input").next().remove();
-				$("#search-recipes-rated-by-user-input").after("<p class='error'>No se ha añadido un número" +
-					" válido</p>");
-				throw new Error(numeroInvalido);
+			if (input.next().length === 0) {
+				input.next().remove();
 			}
-		}
+			if (results.length === 0) {
+				const p = document.createElement("p");
+				p.textContent = "No se han encontrado recetas";
 
-		if ($("#search-recipes-rated-by-user-input").next().length === 0) {
-			$("#search-recipes-rated-by-user-input").next().remove();
-		}
-		if (results.length === 0) {
-			const p = document.createElement("p");
-			p.textContent = "No se han encontrado recetas";
-
-			resultsDiv.appendChild(p);
+				resultsDiv.appendChild(p);
+			} else {
+				results
+					.map(recipeToDiv)
+					.forEach(it => resultsDiv.appendChild(it));
+			}
 		} else {
-			results
-				.map(recipeToDiv)
-				.forEach(it => resultsDiv.appendChild(it));
+			marcarError();
 		}
 	});
 }
